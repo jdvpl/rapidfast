38292334bbd24c24d9135cb0be514bfb33144483
Juan Daniel <juanda554242@gmail.com>
2020-11-29 17:34:26 -0500

    IOmplementando las librerias de firebase para conectase

--- app/src/main/java/com/jdrapid/rapidfast/LoginActivity.java
+++ app/src/main/java/com/jdrapid/rapidfast/LoginActivity.java
@@ -3,12 +3,39 @@ package com.jdrapid.rapidfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextInputEditText InEmail,InContrasena;
+    Button BtnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        InEmail=findViewById(R.id.txtInputCorrero);
+        InContrasena=findViewById(R.id.txtInputContrasena);
+        BtnLogin=findViewById(R.id.BtnLogin);
+        
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login();
+            }
+        });
+
+    }
+
+    private void Login() {
+        String correo=InEmail.getText().toString();
+        String contrasena=InContrasena.getText().toString();
+        if (!correo.isEmpty() && !contrasena.isEmpty()){
+            if (contrasena.length()>=6){
+
+            }
+
+        }
     }
 }
--- app/src/main/java/com/jdrapid/rapidfast/SeletAuthOptionActivity.java
+++ app/src/main/java/com/jdrapid/rapidfast/SeletAuthOptionActivity.java
@@ -3,10 +3,14 @@ package com.jdrapid.rapidfast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SeletAuthOptionActivity extends AppCompatActivity {
+    Button BtnLogin;
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,18 @@ public class SeletAuthOptionActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.select_toolbar_opcion);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        BtnLogin=findViewById(R.id.BtnLogin);
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iraLogin();
+            }
+        });
+    }
+
+    private void iraLogin() {
+        Intent intent=new Intent(SeletAuthOptionActivity.this,LoginActivity.class);
+        startActivity(intent);
     }
 }
