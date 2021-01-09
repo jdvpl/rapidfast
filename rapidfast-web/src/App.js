35e22862aa55178558b5719033294d9d1e3ec548
Juan Daniel <juanda554242@gmail.com>
2021-01-09 10:19:10 -0500

    isntalando el router dom npm i react-router@next react-router-dom@next

--- rapidfast-web/src/App.js
+++ rapidfast-web/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="bg-black h-100">
+      <h1 className="text-4xl font-bold text-center text-white">
+      Rapid Fast
+    </h1>
     </div>
+   
   );
 }
 
