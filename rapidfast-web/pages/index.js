325bf05d58262f8ff08d426a06b34aa49a60b9df
Juan Daniel <juanda554242@gmail.com>
2021-01-23 13:10:21 -0500

    proyecto de rapid fast wev con next

---
+++ rapidfast-web/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React from 'react';
+const Home = () => {
+  return ( 
+    <h1>Hola Rapid Fast</h1>
+   );
+}
+ 
+export default Home;
